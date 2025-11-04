3c2eb0264571e72e20a68faa949b526b1c037d7b
George <P>
2025-11-03 22:41:19 -0800

    added ILogger & ILoggerFactory
    added TestOutputLoggerProvider
    
    adjusted logging

--- src/Html2x/ILayoutBuilderFactory.cs
+++ src/Html2x/ILayoutBuilderFactory.cs
@@ -1,8 +1,9 @@
 using Html2x.Layout;
+using Microsoft.Extensions.Logging;
 
 namespace Html2x;
 
 public interface ILayoutBuilderFactory
 {
-    LayoutBuilder Create();
+    LayoutBuilder Create(ILoggerFactory? loggerFactory = null);
 }
