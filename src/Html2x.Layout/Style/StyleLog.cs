b075bb1b34572d9ddbc063a600a177aa12b86536
George P <georgespok@gmail.com>
2025-11-06 22:20:38 -0800

    Log unsupported units

--- src/Html2x.Layout/Style/StyleLog.cs
+++ src/Html2x.Layout/Style/StyleLog.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using AngleSharp.Dom;
 using Microsoft.Extensions.Logging;
 
@@ -17,6 +18,12 @@ internal static class StyleLog
             new EventId(3001, nameof(NegativePaddingValue)),
             "Negative padding value '{Value}' for property '{Property}' on element '{ElementTag}'. Using default value 0.");
 
+    private static readonly Action<ILogger, string, string, string, string, Exception?> UnsupportedPaddingUnitMessage =
+        LoggerMessage.Define<string, string, string, string>(
+            LogLevel.Warning,
+            new EventId(3002, nameof(UnsupportedPaddingUnit)),
+            "Unsupported padding unit '{Unit}' in value '{Value}' for property '{Property}' on element '{ElementTag}'. Only 'px' units are supported. Using default value 0.");
+
     public static void InvalidPaddingValue(ILogger logger, string property, string value, IElement element)
     {
         if (logger.IsEnabled(LogLevel.Warning))
@@ -29,7 +36,15 @@ internal static class StyleLog
     {
         if (logger.IsEnabled(LogLevel.Warning))
         {
-            NegativePaddingValueMessage(logger, value.ToString(), property, element.TagName, null);
+            NegativePaddingValueMessage(logger, value.ToString(CultureInfo.InvariantCulture), property, element.TagName, null);
+        }
+    }
+
+    public static void UnsupportedPaddingUnit(ILogger logger, string property, string value, string unit, IElement element)
+    {
+        if (logger.IsEnabled(LogLevel.Warning))
+        {
+            UnsupportedPaddingUnitMessage(logger, unit, value, property, element.TagName, null);
         }
     }
 }
