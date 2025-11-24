bb27c287b908f40df1a0f3e5b8627143548efe87
George P <georgespok@gmail.com>
2025-11-23 17:22:26 -0800

    Improved TestConsole ui

--- src/Tests/Html2x.TestConsole/Program.cs
+++ src/Tests/Html2x.TestConsole/Program.cs
@@ -4,14 +4,6 @@ namespace Html2x.TestConsole;
 
 internal static class Program
 {
-    public static Task<int> Main(string[] args)
-    {
-        var app = new CommandApp<RenderCommand>();
-        app.Configure(config =>
-        {
-            config.SetApplicationName("Html2x.TestConsole");
-        });
-
-        return app.RunAsync(args);
-    }
+    public static Task<int> Main(string[] args) => 
+        new CommandApp<RenderCommand>().RunAsync(args);
 }
